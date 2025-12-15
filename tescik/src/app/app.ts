21e97afee0d34ace8d56d4bf48ec342698f6032b
Fabian Wręczycki <f.wreczycki@wasko.pl>
2025-12-15 10:13:55 +0100

    Zmiana z spotify auth service do callbacku

--- tescik/src/app/app.ts
+++ tescik/src/app/app.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SpotifyAuthService } from './spotify-auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
 })
-export class App {
+export class App implements OnInit {
+  isLogged:boolean = true;
+
   constructor(private auth: SpotifyAuthService) {}
+
+  ngOnInit(): void{
+    this.isLogged = this.getIsLogged();
+  }
+
+
 
   login() {
     this.auth.login();
+  
   }
+
+private getIsLogged(): boolean {
+  const token = localStorage.getItem('spotify_access_token');
+  const expiresAt = localStorage.getItem('spotify_expires_at');
+  if (!token || !expiresAt) {
+    return false;
+  }
+
+  return Date.now() < Number(expiresAt);
+}
+
 }
--- tescik/src/app/spotify-auth.service.ts
+++ tescik/src/app/spotify-auth.service.ts
@@ -5,26 +5,27 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SpotifyAuthService {
+
   private clientId = '2b02fb02a65c4245ab01b4f55578cfa9';
+  private clientSecret = '50beb5956e0d425c9c252e0981901b91';
   private redirectUri = 'http://127.0.0.1:4200/callback';
 
   constructor(private http: HttpClient) {}
 
   login() {
-    const scope = 'user-read-private user-read-email';
-    const authUrl = new URL('https://accounts.spotify.com/authorize');
-    authUrl.search = new URLSearchParams({
-      response_type: 'code',
-      client_id: this.clientId,
-      redirect_uri: this.redirectUri,
-      scope
-    }).toString();
+    const scopes = 'user-read-email user-read-private';
 
-    window.location.href = authUrl.toString();
+    const authUrl =
+      'https://accounts.spotify.com/authorize?' +
+      'client_id=' + this.clientId +
+      '&response_type=code' +
+      '&redirect_uri=' + encodeURIComponent(this.redirectUri) +
+      '&scope=' + encodeURIComponent(scopes);
+
+    window.location.href = authUrl;
   }
 
-  // HttpClient 
-  async getAccessToken(code: string) {
+  getAccessToken(code: string) {
     const url = 'https://accounts.spotify.com/api/token';
 
     const body = new HttpParams()
@@ -32,36 +33,12 @@ export class SpotifyAuthService {
       .set('code', code)
       .set('redirect_uri', this.redirectUri)
       .set('client_id', this.clientId)
-      .set('client_secret', '50beb5956e0d425c9c252e0981901b91');
+      .set('client_secret', this.clientSecret);
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded'
     });
 
-    this.http.post(url, body.toString(), { headers }).subscribe({
-    next: (response: any) => {
-
-      const accessToken = response.access_token;
-      const refreshToken = response.refresh_token;
-      const expiresIn = response.expires_in;
-
-      console.log('Access token:', accessToken);
-      console.log('Refresh token:', refreshToken);
-      console.log('Wygasa (sekundy):', expiresIn);
-
-      const expiresAt = Date.now() + expiresIn * 1000;
-      console.log('Token wygasa:', new Date(expiresAt).toLocaleString());
-
-      localStorage.setItem('spotify_access_token', accessToken);
-      localStorage.setItem('spotify_expires_at', expiresAt.toString());
-
-      if (refreshToken) {
-        localStorage.setItem('spotify_refresh_token', refreshToken);
-      }
-    },
-    error: (error) => {
-      console.error('❌ Błąd podczas pobierania tokena:', error);
-    }
-  });
+    return this.http.post(url, body.toString(), { headers });
   }
 }
